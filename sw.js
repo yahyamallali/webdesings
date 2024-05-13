cc7ee38a44267ed292201f2d050dd8b61006daa1
yahya mallali <67528823+yahyamallali@users.noreply.github.com>
2024-05-13 17:53:52 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,45 @@
-{
-    "short_name": "app yahya",
-    "name": "yahya app",
-    "icons": [
-      {
-        "src": "/images/icons-192.png",
-        "type": "image/png",
-        "sizes": "192x192"
-      },
-      {
-        "src": "/images/icons-512.png",
-        "type": "image/png",
-        "sizes": "512x512"
-      }
-    ],
-    "start_url": "/index.html",
-    "background_color": "#000000",
-    "display": "standalone",
-    "theme_color": "#ff0000"
-  }
+
+
+// On install - caching the application shell and additional paths
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('sw-cache').then(function(cache) {
+      // Cache the application shell (index.html) and additional paths
+      return cache.addAll([
+        'index.html',
+        '1.html',
+        '9alawa.html',
+        'daimo.png',
+        'doros.html',
+        'fileList.json',
+        'index-2.html',
+        'index.js',
+        'LICENSE',
+        'list.png',
+        'listFiles.js',
+        'manifest.json',
+        'package-lock.json',
+        'package.json',
+        'page doros',
+        'Page2.html',
+        'README.md',
+        'school.png',
+        'sw.js',
+        'test.html',
+        'wataniyat folder'
+        // Add more paths here as needed
+      ]);
+    })
+  );
+});
+
+// On network request
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    // Try the cache
+    caches.match(event.request).then(function(response) {
+      // If response found, return it; else, fetch again
+      return response || fetch(event.request);
+    })
+  );
+});
