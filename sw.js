a4e98dc6ffb5e92c4a4cf2c221121212e58fa631
yahya mallali <67528823+yahyamallali@users.noreply.github.com>
2024-05-13 17:41:11 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,9 +1,34 @@
-// On install - caching the application shell
+
+
+// On install - caching the application shell and additional paths
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open('sw-cache').then(function(cache) {
-      // cache any static files that make up the application shell
-      return cache.add('index.html');
+      // Cache the application shell (index.html) and additional paths
+      return cache.addAll([
+        'index.html',
+        '1.html',
+        '9alawa.html',
+        'daimo.png',
+        'doros.html',
+        'fileList.json',
+        'index-2.html',
+        'index.js',
+        'LICENSE',
+        'list.png',
+        'listFiles.js',
+        'manifest.json',
+        'package-lock.json',
+        'package.json',
+        'page doros',
+        'Page2.html',
+        'README.md',
+        'school.png',
+        'sw.js',
+        'test.html',
+        'wataniyat folder'
+        // Add more paths here as needed
+      ]);
     })
   );
 });
@@ -13,7 +38,7 @@ self.addEventListener('fetch', function(event) {
   event.respondWith(
     // Try the cache
     caches.match(event.request).then(function(response) {
-      //If response found return it, else fetch again
+      // If response found, return it; else, fetch again
       return response || fetch(event.request);
     })
   );
