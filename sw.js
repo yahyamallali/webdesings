09517c14fd2da18326f75db32a2d520c064d6319
yahya mallali <67528823+yahyamallali@users.noreply.github.com>
2024-05-13 17:47:05 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,45 +1,58 @@
+{
+  "short_name": "app yahya",
+  "name": "yahya app",
+  "icons": [
+    {
+      "src": "/images/icons-192.png",
+      "type": "image/png",
+      "sizes": "192x192"
+    },
+    {
+      "src": "/images/icons-512.png",
+      "type": "image/png",
+      "sizes": "512x512"
+    }
+  ],
+  "start_url": "/index.html",
+  "background_color": "#000000",
+  "display": "standalone",
+  "theme_color": "#ff0000",
+  
+  "files_and_directories": [
+    {"name":"1.html","type":"file"},
+    {"name":"9alawa.html","type":"file"},
+    {"name":"/assest-foot","type":"directory"},
+    {"name":"/assets","type":"directory"},
+    {"name":"/daimo.png","type":"file"},
+    {"name":"/doros.html","type":"file"},
+    {"name":"/fileList.json","type":"file"},
+    {"name":"/images","type":"directory"},
+    {"name":"/index-2.html","type":"file"},
+    {"name":"/index.html","type":"file"},
+    {"name":"/index.js","type":"file"},
+    {"name":"/LICENSE","type":"file"},
+    {"name":"/list.png","type":"file"},
+    {"name":"/listFiles.js","type":"file"},
+    {"name":"/manifest.json","type":"file"},
+    {"name":"/package-lock.json","type":"file"},
+    {"name":"/package.json","type":"file"},
+    {"name":"/page doros","type":"file"},
+    {"name":"Page2.html","type":"file","path": "/"},
+    {"name":"/pages doros","type":"directory"},
+    {"name":"/README.md","type":"file"},
+    {"name":"school.png","type":"file"},
+    {"name":"sw.js","type":"file"},
+    {"name":"test.html","type":"file"},
+    {"name":"wataniyat folder","type":"directory"}
+  ], "pdf_files": [
+    {"name": "watani-2015.pdf", "type": "file", "path": "/wataniyat/watani-svt-pdf/"},
+    {"name": "watani-2016.pdf", "type": "file", "path": "/wataniyat/watani-svt-pdf/"},
+    {"name": "watani-2017.pdf", "type": "file", "path": "/wataniyat/watani-svt-pdf/"},
+    {"name": "watani-2018.pdf", "type": "file", "path": "/wataniyat/watani-svt-pdf/"},
+    {"name": "watani-2019.pdf", "type": "file", "path": "/wataniyat/watani-svt-pdf/"},
+    {"name": "watani-2020.pdf", "type": "file", "path": "/wataniyat/watani-svt-pdf/"},
+    {"name": "watani-2021.pdf", "type": "file", "path": "/wataniyat/watani-svt-pdf/"}
+  ]
+}
+  
 
-
-// On install - caching the application shell and additional paths
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('sw-cache').then(function(cache) {
-      // Cache the application shell (index.html) and additional paths
-      return cache.addAll([
-        'index.html',
-        '1.html',
-        '9alawa.html',
-        'daimo.png',
-        'doros.html',
-        'fileList.json',
-        'index-2.html',
-        'index.js',
-        'LICENSE',
-        'list.png',
-        'listFiles.js',
-        'manifest.json',
-        'package-lock.json',
-        'package.json',
-        'page doros',
-        'Page2.html',
-        'README.md',
-        'school.png',
-        'sw.js',
-        'test.html',
-        'wataniyat folder'
-        // Add more paths here as needed
-      ]);
-    })
-  );
-});
-
-// On network request
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    // Try the cache
-    caches.match(event.request).then(function(response) {
-      // If response found, return it; else, fetch again
-      return response || fetch(event.request);
-    })
-  );
-});
